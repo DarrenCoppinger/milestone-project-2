037f38fed282b7e041607c9577815ef61306b1d5
Ubuntu <ubuntu@ip-172-31-85-155.ec2.internal>
2019-09-18 22:36:40 +0000

    fixed EmailJS bug stopping page reloading on submission

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,21 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "example", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                document.getElementById("myform").reset();
+                // document.getElementById("fullname").value=" ";
+                // document.getElementById("emailaddress").value=" ";
+                // document.getElementById("projectsummary").value=" ";
+
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
