b01157508d6772148f6d718b4063e478a9572e9c
Ubuntu <ubuntu@ip-172-31-85-155.ec2.internal>
2019-09-22 21:27:41 +0000

    fixed bug which stopped clustered markers displaying infoWindows when clicked

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,14 +1,36 @@
     function initMap() {
 
         var styledMapType = new google.maps.StyledMapType(
-            [
-                { "elementType": "geometry", "stylers": [{ "color": "#ebe3cd" }] },
-                { "elementType": "labels.text.fill", "stylers": [{ "color": "#523735" }] },
-                { "elementType": "labels.text.stroke", "stylers": [{ "color": "#f5f1e6" }] },
+            [{
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#ebe3cd"
+                    }]
+                },
+                {
+                    "elementType": "labels.text.fill",
+                    "stylers": [{
+                        "color": "#523735"
+                    }]
+                },
+                {
+                    "elementType": "labels.text.stroke",
+                    "stylers": [{
+                        "color": "#f5f1e6"
+                    }]
+                },
                 {
                     "featureType": "administrative",
                     "elementType": "geometry.stroke",
-                    "stylers": [{ "color": "#c9b2a6" }]
+                    "stylers": [{
+                        "color": "#c9b2a6"
+                    }]
+                },
+                {
+                    "featureType": "administrative.land_parcel",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
                 },
                 {
                     "featureType": "administrative.land_parcel",
@@ -22,6 +44,12 @@
                     "elementType": "labels.text.fill",
                     "stylers": [{
                         "color": "#ae9e90"
+                    }]
+                },
+                {
+                    "featureType": "administrative.neighborhood",
+                    "stylers": [{
+                        "visibility": "off"
                     }]
                 },
                 {
@@ -40,9 +68,22 @@
                 },
                 {
                     "featureType": "poi",
+                    "elementType": "labels.text",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "poi",
                     "elementType": "labels.text.fill",
                     "stylers": [{
                         "color": "#93817c"
+                    }]
+                },
+                {
+                    "featureType": "poi.business",
+                    "stylers": [{
+                        "visibility": "off"
                     }]
                 },
                 {
@@ -61,15 +102,23 @@
                 },
                 {
                     "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#f5f1e6"
+                    }]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels",
                     "stylers": [{
                         "visibility": "off"
                     }]
                 },
                 {
                     "featureType": "road",
-                    "elementType": "geometry",
+                    "elementType": "labels.icon",
                     "stylers": [{
-                        "color": "#f5f1e6"
+                        "visibility": "off"
                     }]
                 },
                 {
@@ -115,6 +164,12 @@
                     }]
                 },
                 {
+                    "featureType": "transit",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
                     "featureType": "transit.line",
                     "elementType": "geometry",
                     "stylers": [{
@@ -151,6 +206,13 @@
                 },
                 {
                     "featureType": "water",
+                    "elementType": "labels.text",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "water",
                     "elementType": "labels.text.fill",
                     "stylers": [{
                         "color": "#92998d"
@@ -174,85 +236,98 @@
         map.mapTypes.set('styled_map', styledMapType);
         map.setMapTypeId('styled_map');
 
+        // var locations = [
+        //     { lat: 53.2709613, lng: -9.0531041 }, //"Druid Theatre Company - Mick Lally Theatre"
+        //     { lat: 53.2730267, lng: -9.0546251 }, //"Nora Barnacle's House
+        //     { lat: 53.1031496, lng: -8.7750506 }, //"Thoor Ballylee Yeats Tower" ,
+        //     { lat: 53.0843409, lng: -9.6124936 }, //"Cathaoir Synge"
+        //     { lat: 54.2465530, lng: -8.358062 }, //"Lake Isle of Innisfree" , 
+        //     { lat: 54.3666661, lng: -8.4788218 }, //"Benbulbin", ,
+        //     { lat: 53.2713610, lng: -9.056890 }, //"Lissadell House"
+        //     { lat: 54.2721304, lng: -8.4772024 }, //"Yeats Building" ,
+        //     { lat: 52.4447289, lng: -9.4862804 }, //"Kerry Writers Museum"
+        //     { lat: 52.4470431, lng: -9.4856969 }, //"John B. Keane's Pub"
+        //     { lat: 54.7866719, lng: -8.2833222 } //"Glenties Cemetery - Resting place of Playwright Brian Friel"
+        // ];
         var locations = [
             ["Druid Theatre Company - Mick Lally Theatre", 53.2709613, -9.0531041],
             ["Nora Barnacle's House", 53.2730267, -9.0546251],
-            ["Thoor Ballylee Yeats Tower", 53.271361, -9.056890],
-            ["Cathaoir Synge", 53.271361, -9.056890],
-            ["Lake Isle of Innisfree", 54.2262853, -8.4756664],
-            ["Benbulbin", 53.271361, -9.056890],
-            ["Lissadell House", 53.271361, -9.056890],
-            ["Yeats Building", 54.2457488, -6.9885574],
-            ["Kerry Writers Museum", 52.4447289, -9.4862804],
+            ["Thoor Ballylee Yeats Tower", 53.1031496, -8.7750506],
+            ["Cathaoir Synge", 53.0843409, -9.6124936],
+            ["Lake Isle of Innisfree", 54.2465530, -8.358062],
+            ["Benbulbin", 54.3666661, -8.4788218],
+            ["Lissadell House",54.3467148,-8.583131],
+            ["Yeats Building", 54.2721304, -8.4772024],
+            ["Kerry Writers Museum", 52.4447289,  -9.4862804],
             ["John B. Keane's Pub", 52.4470431, -9.4856969],
             ["Glenties Cemetery - Resting place of Playwright Brian Friel", 54.7866719, -8.2833222]
         ];
 
         var content = [
-            ["<div>" +
+            ["Druid Theatre Company - Mick Lally Theatre", "<div>" +
                 "<h3>Mick Lally Theatre - Druid Theatre Company</h3>" +
                 "<p>Druid was founded in Galway in 1975 by Garry Hynes, Marie Mullen and Mick Lally, the first professional Irish theatre company outside of Dublin.</p>" +
                 "<p>Over the years, Druid has worked with actors, designers, directors, writers, producers and administrators, many of whom have each gone on to play leading roles in Irish and international theatre and all of whom have contributed to the shape of the company. The core staff is based at our offices on Flood Street, Galway.</p>" +
                 "<p><a class='btn btn-sm' href='www.druid.ie' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Nora Barnacle's House", "<div>" +
                 "<h3>Nora Barnacle's House</h3>" +
                 "<p>This terrace house is located in Galway City opposite of St Nicholas Church. It was built in the late 1800s and was home to James Joyce’s wife, Nora Barnacle. Joyce also spent considerable time here when he was writing in the 20th century. The building has been restored, but it’s admittedly showing some signs of wear.</p>" +
                 "<p>Joyce used the house and the tragic tale of Michael Feeney based a section the final short story in Dubliners,'The Dead'. Michael Feeney wason these incidents. Barnacle and Feeney fell in love as teenager before he of typhoid and pneumonia.</p>" +
                 "<p><a class='btn btn-sm' href='http://www.irishtourist.com/details/nora-barnacle-house-museum.shtml' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Thoor Ballylee Yeats Tower", "<div>" +
                 "<h3>Thoor Ballylee Yeats Tower</h3>" +
                 "<p>This urteenth century Hiberno-Norman toweris closely associated with the poet WB Yeats</p>" +
                 "<p><a class='btn btn-sm' href='https://yeatsthoorballylee.org/visit/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Cathaoir Synge", "<div>" +
                 "<h3>Cathaoir Synge</h3>" +
                 "<p>Cathaoir Synge or 'Synge Chair' was a location used to write by the writer J.M. Synge who had a long association with the Aran Islands.  He lived on Inis Meáin for considerable time between 1898 and 1902 and drew inspiration from the island culture. Many of Synge most famous works, e.g. ‘Riders To The Sea’ and ‘The Playboy of The Western World were writtten at this spot.</p>" +
                 "< p > <a class='btn btn-sm' href='http://www.aranislands.ie/cathaoir-synge/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> < /p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Lake Isle of Innisfree", "<div>" +
                 "<h3>Lake Isle of Innisfree</h3>" +
                 "<p>The Lake Isle of Innisfree a beautiful island just off the south coast of Lough Gill. It was the inspiration for the peom by WB Yeats of the same name.</p>" +
-                "< p > <a class='btn btn-sm' href='https://sligotours.ie/locations/lake-isle-of-innisfree/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "<p> <a class='btn btn-sm' href='https://sligotours.ie/locations/lake-isle-of-innisfree/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Benbulbin", "<div>" +
                 "<h3>Ben bulbin</h3>" +
                 "<p>Ben bulbin, sometimes spelled Benbulbin or Benbulben, is a large flat-topped rock formation in County Sligo, Ireland. It is part of the Dartry Mountains, in an area sometimes called 'Yeats Country'. Benbulbin is featured in WB Yeats peom 'Under Ben Bulben'.</p>" +
-                "< p > <a class='btn btn-sm' href='https://sligowalks.ie/walks/benbulbin-gortarowey-looped-walk/' target='_blank'><span class='sr-only'>Walking Tour</span>Walking Tour</a> </p>" +
+                "<p> <a class='btn btn-sm' href='https://sligowalks.ie/walks/benbulbin-gortarowey-looped-walk/' target='_blank'><span class='sr-only'>Walking Tour</span>Walking Tour</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Lissadell House", "<div>" +
                 "<h3>Lissadell House</h3>" +
                 "<p>Lissadell House is a neo-classical Greek revivalist style country house located in County Silgo. </p>" +
                 "<p>The house was a holiday retreat of WB Yeats who imortalised it with the opening lines of his poem: 'In Memory of Eva Gore-Booth and Con Markiewicz' </p>" +
-                "< p > <a class='btn btn-sm' href='http://lissadellhouse.com/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "<p> <a class='btn btn-sm' href='http://lissadellhouse.com/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Yeats Building", "<div>" +
                 "<h3>Yeats Building - Yeats Society</h3>" +
                 "<p>The Yeats Building is centrally located on Hyde Bridge, in Sligo Town Centre. This beautiful building is the home of the Yeats Society, Sligo since 1973.</p>" +
                 "<p> <a class='btn btn-sm' href='https://www.yeatssociety.com/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Kerry Writers Museum", "<div>" +
                 "<h3>Kerry Writers Museum</h3>" +
                 "<p>Kerry Writers’ Museum collects, safeguards, holds in trust, displays and interprets a variety of material and objects relating to the rich literary heritage of County Kerry, and the cultural, social & political heritage of Listowel & North Kerry.</p>" +
                 "<p> <a class='btn btn-sm' href='http://www.kerrywritersmuseum.com' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["John B. Keane's Pub", "<div>" +
                 "<h3>John B Keane's Pub</h3>" +
                 "<p>This pub was previously owned and run by renowned playwrite John B Keane. At the counter, after closing time, Keane wrote his many masterpieces. The pub is now run by Keane's son Billy, who is also a writer and broadcaster.</p>" +
                 "<p> <a class='btn btn-sm' href='http://www.gokerry.ie/businesses/john-b-keanes-bar/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
                 "</div>"
             ],
-            ["<div>" +
+            ["Glenties Cemetery - Resting place of Playwright Brian Friel", "<div>" +
                 "<h3>Glenties Cemetery</h3>" +
                 "<p>Resting place of Playwright Brian Friel</p>" +
                 "</div>"
@@ -260,26 +335,85 @@
         ];
 
         // Initialise Markers on Map 
-        for (i = 0; i < locations.length; i++) {
-            var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
 
+        // var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
+
+        // var marker = new google.maps.Marker({
+        //     position: position,
+        //     map: map,
+        //     title: locations[i][0]
+        // });
+
+
+        // // Mark 1
+        // var marker = locations.map(function(location, i) {
+        //     return new google.maps.Marker({
+        //         position: location,
+        //         map: map,
+        //         title: content[i][0]
+        //     });
+        // });
+        // var gmarker = [];
+        // var infowindow = new google.maps.InfoWindow();
+        // google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        //     return function() {
+        //         infowindow.setContent(content[i][1]);
+        //         // infowindow.setOptions({ maxWidth: 200 });
+        //         infowindow.open(map, marker);
+        //     };
+        // })(marker, i));
+        // gmarker.push(marker);
+
+        // var markerCluster = new MarkerClusterer(map, gmarker, {
+        //     imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        // });
+
+        // // Mark 2
+        // var gmarker = [];
+        // var infowindow = new google.maps.InfoWindow();
+        // for (i = 0; i < locations.length; i++) {
+        //     var marker = new google.maps.Marker({
+        //         position: locations[i][0],
+        //         map: map,
+        //         title: content[i][0]
+        //     });
+
+        //     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        //         return function() {
+        //             infowindow.setContent(content[i][1]);
+        //             // infowindow.setOptions({ maxWidth: 200 });
+        //             infowindow.open(map, marker);
+        //         };
+        //     })(marker, i));
+        //     gmarker.push(marker);
+        // }
+
+        // var markerCluster = new MarkerClusterer(map, gmarker, {
+        //     imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        // });
+        // Mark 3
+
+        var gmarkers = [];
+        var infowindow = new google.maps.InfoWindow({});
+        for (var i = 0; i < locations.length; i++) {
             var marker = new google.maps.Marker({
-                position: position,
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
                 map: map,
                 title: locations[i][0]
             });
 
-            var infowindow = new google.maps.InfoWindow();
             google.maps.event.addListener(marker, 'click', (function(marker, i) {
                 return function() {
-                    infowindow.setContent(content[i][0]);
-                    // infowindow.setOptions({ maxWidth: 200 });
+                    infowindow.setContent(content[i][1]);
                     infowindow.open(map, marker);
                 };
             })(marker, i));
+            gmarkers.push(marker);
         }
-        var markerCluster = new MarkerClusterer(map, marker, {
+
+        var markerCluster = new MarkerClusterer(map, gmarkers, {
             imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
         });
+
     }
     
--- rw/rw.js
+++ rw/rw.js
@@ -73,3 +73,91 @@
         //         "</div>"
         //     ],
         // ];
+        
+        
+        
+        
+                var locations = [
+            ["Druid Theatre Company - Mick Lally Theatre", 53.2709613, -9.0531041],
+            ["Nora Barnacle's House", 53.2730267, -9.0546251],
+            ["Thoor Ballylee Yeats Tower", 53.271361, -9.056890],
+            ["Cathaoir Synge", 53.271361, -9.056890],
+            ["Lake Isle of Innisfree", 54.2262853, -8.4756664],
+            ["Benbulbin", 53.271361, -9.056890],
+            ["Lissadell House", 53.271361, -9.056890],
+            ["Yeats Building", 54.2457488, -6.9885574],
+            ["Kerry Writers Museum", 52.4447289, -9.4862804],
+            ["John B. Keane's Pub", 52.4470431, -9.4856969],
+            ["Glenties Cemetery - Resting place of Playwright Brian Friel", 54.7866719, -8.2833222]
+        ];
+
+        var content = [
+            ["<div>" +
+                "<h3>Mick Lally Theatre - Druid Theatre Company</h3>" +
+                "<p>Druid was founded in Galway in 1975 by Garry Hynes, Marie Mullen and Mick Lally, the first professional Irish theatre company outside of Dublin.</p>" +
+                "<p>Over the years, Druid has worked with actors, designers, directors, writers, producers and administrators, many of whom have each gone on to play leading roles in Irish and international theatre and all of whom have contributed to the shape of the company. The core staff is based at our offices on Flood Street, Galway.</p>" +
+                "<p><a class='btn btn-sm' href='www.druid.ie' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Nora Barnacle's House</h3>" +
+                "<p>This terrace house is located in Galway City opposite of St Nicholas Church. It was built in the late 1800s and was home to James Joyce’s wife, Nora Barnacle. Joyce also spent considerable time here when he was writing in the 20th century. The building has been restored, but it’s admittedly showing some signs of wear.</p>" +
+                "<p>Joyce used the house and the tragic tale of Michael Feeney based a section the final short story in Dubliners,'The Dead'. Michael Feeney wason these incidents. Barnacle and Feeney fell in love as teenager before he of typhoid and pneumonia.</p>" +
+                "<p><a class='btn btn-sm' href='http://www.irishtourist.com/details/nora-barnacle-house-museum.shtml' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Thoor Ballylee Yeats Tower</h3>" +
+                "<p>This urteenth century Hiberno-Norman toweris closely associated with the poet WB Yeats</p>" +
+                "<p><a class='btn btn-sm' href='https://yeatsthoorballylee.org/visit/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Cathaoir Synge</h3>" +
+                "<p>Cathaoir Synge or 'Synge Chair' was a location used to write by the writer J.M. Synge who had a long association with the Aran Islands.  He lived on Inis Meáin for considerable time between 1898 and 1902 and drew inspiration from the island culture. Many of Synge most famous works, e.g. ‘Riders To The Sea’ and ‘The Playboy of The Western World were writtten at this spot.</p>" +
+                "< p > <a class='btn btn-sm' href='http://www.aranislands.ie/cathaoir-synge/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> < /p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Lake Isle of Innisfree</h3>" +
+                "<p>The Lake Isle of Innisfree a beautiful island just off the south coast of Lough Gill. It was the inspiration for the peom by WB Yeats of the same name.</p>" +
+                "< p > <a class='btn btn-sm' href='https://sligotours.ie/locations/lake-isle-of-innisfree/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Ben bulbin</h3>" +
+                "<p>Ben bulbin, sometimes spelled Benbulbin or Benbulben, is a large flat-topped rock formation in County Sligo, Ireland. It is part of the Dartry Mountains, in an area sometimes called 'Yeats Country'. Benbulbin is featured in WB Yeats peom 'Under Ben Bulben'.</p>" +
+                "< p > <a class='btn btn-sm' href='https://sligowalks.ie/walks/benbulbin-gortarowey-looped-walk/' target='_blank'><span class='sr-only'>Walking Tour</span>Walking Tour</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Lissadell House</h3>" +
+                "<p>Lissadell House is a neo-classical Greek revivalist style country house located in County Silgo. </p>" +
+                "<p>The house was a holiday retreat of WB Yeats who imortalised it with the opening lines of his poem: 'In Memory of Eva Gore-Booth and Con Markiewicz' </p>" +
+                "< p > <a class='btn btn-sm' href='http://lissadellhouse.com/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Yeats Building - Yeats Society</h3>" +
+                "<p>The Yeats Building is centrally located on Hyde Bridge, in Sligo Town Centre. This beautiful building is the home of the Yeats Society, Sligo since 1973.</p>" +
+                "<p> <a class='btn btn-sm' href='https://www.yeatssociety.com/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Kerry Writers Museum</h3>" +
+                "<p>Kerry Writers’ Museum collects, safeguards, holds in trust, displays and interprets a variety of material and objects relating to the rich literary heritage of County Kerry, and the cultural, social & political heritage of Listowel & North Kerry.</p>" +
+                "<p> <a class='btn btn-sm' href='http://www.kerrywritersmuseum.com' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>John B Keane's Pub</h3>" +
+                "<p>This pub was previously owned and run by renowned playwrite John B Keane. At the counter, after closing time, Keane wrote his many masterpieces. The pub is now run by Keane's son Billy, who is also a writer and broadcaster.</p>" +
+                "<p> <a class='btn btn-sm' href='http://www.gokerry.ie/businesses/john-b-keanes-bar/' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a> </p>" +
+                "</div>"
+            ],
+            ["<div>" +
+                "<h3>Glenties Cemetery</h3>" +
+                "<p>Resting place of Playwright Brian Friel</p>" +
+                "</div>"
+            ],
+        ];
